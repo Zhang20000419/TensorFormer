4af661e04227f28dbefc9271ec047d2c1cc51a66
ZZC's computer <2532214352@qq.com>
2023-12-20 21:54:01 +0800

    write dataloader

---
+++ TensorformerBlock.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from CMU_MOSI_DataLoader import CMU_MOSI_Dataset
+
+
+class TensorFormerBlock(nn.Module):
+    def __init__(self, multimodal_dim):
+        super(TensorFormerBlock, self).__init__()
+        self.
+
+    def forward(self, x):
+        return x
